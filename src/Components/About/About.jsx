16fb4ddf8433496d3bc7a4887e6b3db77cbbde21
James Israel <yongeazi143@gmail.com>
2023-09-23 10:13:17 +0100

    About Component for mobile Completed

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,12 +2,12 @@ import images from "../../images";
 
 const About = () => {
   return (
-    <section className="mx-auto  flex flex-col items-center p-5 justify-between">
+    <section className="mx-auto  flex flex-col items-center p-8 justify-between relative">
       <div className="relative mb-8">
         <img src={images.idea} alt="Idea" className="w-[400px] mt-10" />
         <img src={images.arrow} alt="arrow" className="mx-auto" />
       </div>
-      <div className="text-center">
+      <div className="text-center relative">
         <h2 className="font-sans lg:text-3xl font-bold">
           Introduction to getlinked
           <br />
@@ -24,7 +24,17 @@ const About = () => {
           pushing the boundaries of technology, and creating solutions that can
           change the world, that's what we're all about!
         </p>
+        <img
+          src={images.star4}
+          alt="star4"
+          className="absolute top-8 -right-3 w-4"
+        />
       </div>
+      <img
+        src={images.star3}
+        alt="star3"
+        className="absolute top-48 left-7 w-4"
+      />
     </section>
   );
 };
